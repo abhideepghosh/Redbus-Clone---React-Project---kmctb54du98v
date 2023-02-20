f63bcb81644c09ef1092b224fb5598d11de73637
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-02-20 06:05:01 +0000

    Home Page Structured

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "../styles/App.css";
 import Header from "./Header";
+import Home from "./Home";
+import SearchBus from "./SearchBus";
 const App = () => {
   return (
-    <div id="main">
-      <Header />
+    <div id="main" className="header">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search/:from/:to" element={<SearchBus />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/components/SearchBus.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./../styles/SearchBus.css";
+
+const SearchBus = () => {
+  return <div>SearchBus</div>;
+};
+
+export default SearchBus;
